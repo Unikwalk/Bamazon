3d06df19cb44d181aa02c930c8697b089dcf6c9a
Unikwalk <vo.jennifer.n@gmail.com>
2019-03-15 21:42:40 -0700

    Updated files for Bamazon app.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Node.js-MySQL
+#BAMAZON STOREFRONT APP
+
+(https://github.com/Unikwalk/Bamazon "Bamazon on GitHub")
+
+##Project Descriptions:
+
+Bamazon is like Amazon with a B and it carries much cooler stuff! 
+
+The app will take in orders from customers and deplete stock from the store's inventory. The app is programmed to track product sales across the store's departments and then provide a summary of the highest-grossing departments in the store.
+
+###Customer View:
+
+When the js file is called, using `node bamazon.js`, it will display a complete list of all products, departments, pricing, and current inventory. 
+
+Users has 2 options: Choose to exit by typing 'X' or choose the product by typing in its `item_id`.
+
+* If user types 'X', the app will say "Goodbye" and display the complete list again.
+* If user types in an item id, the app will provide 2 options: To exit or to enter a quantity.
+* If an item id is provided, the app will compare the quantity against the inventory level.
+* If the required quantity is less than inventory, purchase will be made and inventory wil be deducted. The app will inform user and display the list again, this time with updated inventory for that item.
+* If the quantity is more than inventory, purchase will not be made and the app will display the complete list again.
+
+When the js file is called, using `node bamazon.js`, it will display a complete list of all products:
+
+(displayProduct.JPG)
+
+Choose to exit by typing 'X': 
+
+(displayProductX.JPG)
+
+Choose the product by typing in its `item_id`:
+
+(chooseItemID.JPG)
+
+Choose quantity and purchase is successful:
+
+(makePurchase.JPG)
+
+Choose quantity and purchase is unsuccessful:
+
+(checkStock.JPG)
+
+
+
+
+
+
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,119 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "miumiuisawesomE",
+  database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  displayProducts();
+});
+
+//Display all products
+
+function displayProducts() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+
+    runSearch(res);
+  });
+}
+
+function runSearch(stock) {
+  inquirer
+    .prompt({
+      name: "choice",
+      type: "input",
+      message: "What's the ID of the item you would you like to purchase: [Exit with 'X']",
+      //Check if the input is not a number or an "x"
+      validate: function (val) {//val is the answer to the question above
+        return !isNaN(val) || val.toLowerCase() === "x";
+      }
+    })
+    .then(function (val) {
+      //If input is "x"
+      exitFunction(val.choice);
+      var choiceId = parseInt(val.choice); //change the answer from a string to a number
+      var product = checkStock(choiceId, stock);
+
+      // If the item is in stock, ask for quantity
+      if (product) {
+        askForQuantity(product);
+      }
+      else {
+        // Otherwise let them know the item is not in the stock, re-run displayProducts
+        console.log("\nThat item is not in the stock.");
+        displayProducts();
+      }
+    });
+};
+
+function askForQuantity(product) {
+  inquirer
+    .prompt([
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many do you need? [Exit with 'X']",
+        validate: function (val) { //check if the answer to question above is more than 0 or an "x"
+          return val > 0 || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function (val) {
+      //If input is "x"
+      exitFunction(val.quantity);
+      var quantity = parseInt(val.quantity); //change the answer from a string to a number
+
+      //if not enough stock, tell customer and displayProducts again
+      if (quantity > product.stock_quantity) {
+        console.log("\nNot Enough Stock!");
+        displayProducts();
+      }
+      else {
+        // Otherwise run makePurchase, give it the product information and desired quantity to purchase
+        makePurchase(product, quantity);
+      }
+    });
+}
+
+function makePurchase(product, quantity) {
+  connection.query(
+    "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", //update mysql database, subtract the input quantity
+    [quantity, product.item_id],
+    function(err, res) {
+      // Let the user know the purchase was successful, re-run loadProducts
+      console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + "(s)!" + 
+                  "\n" +
+                  "\n-----------------------------"
+                );
+      displayProducts();
+    }
+  );
+} 
+
+function checkStock(choiceId, stock) {
+  for (var i = 0; i < stock.length; i++) {
+    if (stock[i].item_id === choiceId) {
+      // If a matching product is found, return the product
+      return stock[i];
+    }
+  }
+  // Otherwise return null
+  return null;
+}
+
+function exitFunction(choice) {
+  if (choice.toLowerCase() === "x") {
+    console.log("Goodbye!");
+    process.exit(0);
+  }
+}
